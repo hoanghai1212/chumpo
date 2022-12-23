811d663cecc0df7a29dc767be4afa896f07b1080
Hoang Hai <hoanghai129612@gmail.com>
2022-12-23 12:05:26 +0700

    init project

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "ui";
+import { add, Button } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <p>Heres a number: {add(1, 2)}</p>
       <Button />
     </div>
   );
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,7 @@
-import * as React from "react";
 export const Button = () => {
   return <button>Boop</button>;
 };
+
+export const add = (a: number, b: number) => {
+  return a + b;
+}

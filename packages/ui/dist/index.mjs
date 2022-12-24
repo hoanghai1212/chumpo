490eed18fa9d83d020496e4d40e1efd3f3e2f6c3
Trương Hoàng Hải <hoanghai129612@gmail.com>
2022-12-24 10:56:10 +0000

    chore: add vercel.json for ui

--- packages/ui/dist/index.mjs
+++
@@ -1,6 +0,0 @@
-import { jsx as t } from 'react/jsx-runtime';
-var p = ({ children: o, onClick: r }) =>
-  t('button', { className: 'rounded-sm bg-red-200 px-1 py-2', onClick: r, children: o });
-import { jsx as e } from 'react/jsx-runtime';
-var i = ({ version: o }) => e('p', { children: o });
-export { p as Button, i as Version };

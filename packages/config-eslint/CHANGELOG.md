4820b02bedbe2a150e491ae40c2fe4ca50cd5ac4
hoanghai1212 <hoanghai129612@gmail.com>
2022-12-28 08:21:11 +0700

    chore: add npm registry cònig

--- packages/config-eslint/CHANGELOG.md
+++ packages/config-eslint/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @chum/eslint-config
 
+## 1.0.1
+
+### Patch Changes
+
+- publish to github registry
+
 ## 1.0.0
 
 ### Major Changes
--- packages/ui/src/Version/index.tsx
+++
@@ -1,7 +0,0 @@
-export type VersionProps = {
-  version: string;
-};
-
-export const Version = ({ version }: VersionProps) => {
-  return <p>{version}</p>;
-};
